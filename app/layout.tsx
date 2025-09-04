24fbd4bfca5486185f4ab1f0670ae650f9ac9e96
itsTari <tariebisamuel16@gmail.com>
2025-09-04 14:13:23 +0100

    thumbnail component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from "next";
-import {Poppins} from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import "./globals.css"
 
-const poppins  = Poppins({
-  subsets:['latin'],
-  weight:['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-poppins'
-
+const poppins = Poppins({
+  weight: ['300', '400', '500', '600', '700'],
+  subsets: ['latin'],
+  display: 'swap',
 })
 
 
@@ -24,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.variable} font-poppins antialiased`}
+        className={`${poppins.className} font-poppins antialiased`}
       >
         {children}
       </body>
--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,8 +1,52 @@
-import React from 'react'
+'use client'
+import React, {useCallback, useState} from 'react'
+import {useDropzone} from 'react-dropzone'
+import { Button } from './ui/button'
+import { cn, convertFileToUrl, getFileType } from '@/lib/utils'
+import Thumbnail from './Thumbnail'
 
-const FileUploader = () => {
+ interface Props {
+  ownerId:string,
+  accountId:string,
+  className?:string
+ }
+
+const FileUploader = ({ownerId, accountId, className}:Props) => {
+  const [files, setFiles] = useState<File[]>([])
+  const onDrop = useCallback( async (acceptedFiles:File[])=> {
+    setFiles(acceptedFiles)
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
   return (
-    <div>FileUploader</div>
+     <div {...getRootProps()} className='cursor-pointer'>
+      <input {...getInputProps()} />
+      <Button type='button' className={cn('uploader-button', className)}>
+          <img src='/assets/icons/upload.svg' alt='upload' width={24} height={24} className=''/>
+          <p>Upload</p>
+      </Button>
+      {files.length > 0 && (
+        <ul className='uploader-preview-list'>
+          <h4 className='h4 text-light-100'>Uploading</h4>
+          {files.map((file, id)=> {
+            const {type, extension} = getFileType(file.name)
+            return (
+            <li key={`${file.name}-${id}`} className='uploader-preview-item'>
+              <div className='flex items-center gap-3'>
+                <Thumbnail type={type} extension={extension} url={convertFileToUrl(file)}/>
+                
+              </div>
+            </li>)
+          })}
+        </ul>
+        
+      )}
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
   )
 }
 
